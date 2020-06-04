508ee6e1dda4880b71dde3c7a53b33446d4a86c9
Michael Marvive <marvive42@gmail.com>
2020-06-03 17:56:23 -0700

    Turned code into an OOP version

--- Coffee Machine/task/machine/coffee_machine.py
+++ Coffee Machine/task/machine/coffee_machine.py
@@ -1,145 +1,114 @@
-# Write your code here
-# print("""Starting to make a coffee
-# Grinding coffee beans
-# Boiling water
-# Mixing boiled water with crushed coffee beans
-# Pouring coffee into the cup
-# Pouring some milk into the cup
-# Coffee is ready!""")
 
-# cups = int(input())
-# print(f"""For {cups} cups of coffee you will need
-# {cups * 200} ml of water
-# {cups * 50} ml of milk
-# {cups * 15} g of coffee beans""")
+class CoffeeMachine:
+    def __init__(self, water, milk, coffee_beans, disposable_cups, money):
+        self.water = water
+        self.milk = milk
+        self.coffee_beans = coffee_beans
+        self.disposable_cups = disposable_cups
+        self.money = money
 
-# water = int(input("Write how many ml of water the coffee machine has:"))
-# milk = int(input("Write how many ml of water the coffee machine has:"))
-# coffee = int(input("Write how many ml of water the coffee machine has:"))
-# cups = int(input("Write how many ml of water the coffee machine has:"))
-# max_coffee = min([(water // 200), (milk // 50), (coffee // 15)])
-# if cups <= max_coffee:
-#     print(f"Yes, I can make that amount of coffee (and even {max_coffee - cups} more than that)")
-# else:
-#     print(f"No, I can make only {max_coffee} cups of coffee")
+    def start_machine(self):
+        options = ""
+        while options != "exit":
+            options = input("Write action (buy, fill, take, remaining:)")
+            if options == "buy":
+                types = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
+                if types == "1":
+                    self.water -= 250
+                    self.coffee_beans -= 16
+                    self.money += 4
+                    self.disposable_cups -= 1
+                    for each in [self.water, self.milk, self.coffee_beans, self.disposable_cups]:
+                        if each < 0:
+                            if self.water < 0:
+                                message = "water"
+                            elif self.coffee_beans < 0:
+                                message = "coffee beans"
+                            elif self.disposable_cups < 0:
+                                message = "disposable_cups"
+                            else:
+                                message = "Error"
+                            self.water += 250
+                            self.coffee_beans += 16
+                            self.money -= 4
+                            self.disposable_cups += 1
+                            print(f"Sorry, not enough {message}!")
+                            break
 
-amounts = {
-    "water": 400,
-    "milk": 540,
-    "coffee_beans": 120,
-    "disposable_cups": 9,
-    "money": 550,
-}
-# water = 400
-# milk = 540
-# coffee_beans = 120
-# disposable_cups = 9
-# money = 550
+                elif types == "2":
+                    self.water -= 350
+                    self.milk -= 75
+                    self.coffee_beans -= 20
+                    self.money += 7
+                    self.disposable_cups -= 1
+                    for each in [self.water, self.milk, self.coffee_beans, self.disposable_cups]:
+                        if each < 0:
+                            if self.water < 0:
+                                message = "water"
+                            elif self.coffee_beans < 0:
+                                message = "coffee beans"
+                            elif self.disposable_cups < 0:
+                                message = "disposable_cups"
+                            elif self.milk < 0:
+                                message = "milk"
+                            else:
+                                message = "Error"
+                            self.water += 350
+                            self.coffee_beans += 20
+                            self.money -= 7
+                            self.milk += 75
+                            self.disposable_cups += 1
+                            print(f"Sorry, not enough {message}!")
+                            break
+                elif types == "3":
+                    self.water -= 200
+                    self.milk -= 100
+                    self.coffee_beans -= 12
+                    self.money += 6
+                    self.disposable_cups -= 1
+                    # if min(amounts.values()) < 0:
+                    for each in [self.water, self.milk, self.coffee_beans, self.disposable_cups]:
+                        if each < 0:
+                            if self.water < 0:
+                                message = "water"
+                            elif self.coffee_beans < 0:
+                                message = "coffee beans"
+                            elif self.disposable_cups < 0:
+                                message = "disposable_cups"
+                            elif self.milk < 0:
+                                message = "milk"
+                            else:
+                                message = "Error"
+                            self.water += 200
+                            self.coffee_beans += 12
+                            self.money -= 6
+                            self.milk += 100
+                            self.disposable_cups += 1
+                            print(f"Sorry, not enough {message}!")
+                            break
+                elif types == "back":
+                    continue
 
-
-def supplies():
-    print(f"""The coffee machine has:
-    {amounts["water"]} of water
-    {amounts["milk"]} of milk
-    {amounts["coffee_beans"]} of coffee beans
-    {amounts["disposable_cups"]} of disposable cups
-    {amounts["money"]} of money""")
-
-
-options = ""
-while options != "exit":
-    options = input("Write action (buy, fill, take, remaining:)")
-    if options == "buy":
-        types = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
-        if types == "1":
-            amounts["water"] -= 250
-            amounts["coffee_beans"] -= 16
-            amounts["money"] += 4
-            amounts["disposable_cups"] -= 1
-            if min(amounts.values()) < 0:
-                if amounts["water"] < 0:
-                    message = "water"
-                elif amounts["coffee_beans"] < 0:
-                    message = "coffee beans"
-                elif amounts["disposable_cups"] < 0:
-                    message = "disposable_cups"
-                else:
-                    message = "Error"
-                amounts["water"] += 250
-                amounts["coffee_beans"] += 16
-                amounts["money"] -= 4
-                amounts["disposable_cups"] += 1
-                print(f"Sorry, not enough {message}!")
-
-        elif types == "2":
-            amounts["water"] -= 350
-            amounts["milk"] -= 75
-            amounts["coffee_beans"] -= 20
-            amounts["money"] += 7
-            amounts["disposable_cups"] -= 1
-            if min(amounts.values()) < 0:
-                if amounts["water"] < 0:
-                    message = "water"
-                elif amounts["coffee_beans"] < 0:
-                    message = "coffee beans"
-                elif amounts["disposable_cups"] < 0:
-                    message = "disposable_cups"
-                elif amounts["milk"] < 0:
-                    message = "milk"
-                else:
-                    message = "Error"
-                amounts["water"] += 350
-                amounts["coffee_beans"] += 20
-                amounts["money"] -= 7
-                amounts["milk"] += 75
-                amounts["disposable_cups"] += 1
-                print(f"Sorry, not enough {message}!")
-        elif types == "3":
-            amounts["water"] -= 200
-            amounts["milk"] -= 100
-            amounts["coffee_beans"] -= 12
-            amounts["money"] += 6
-            amounts["disposable_cups"] -= 1
-            if min(amounts.values()) < 0:
-                if amounts["water"] < 0:
-                    message = "water"
-                elif amounts["coffee_beans"] < 0:
-                    message = "coffee beans"
-                elif amounts["disposable_cups"] < 0:
-                    message = "disposable_cups"
-                elif amounts["milk"] < 0:
-                    message = "milk"
-                else:
-                    message = "Error"
-                amounts["water"] += 200
-                amounts["coffee_beans"] += 12
-                amounts["money"] -= 6
-                amounts["milk"] += 100
-                amounts["disposable_cups"] += 1
-                print(f"Sorry, not enough {message}!")
-        elif types == "back":
-            continue
-
-    elif options == "fill":
-        water_add = int(input("Write how many ml of water do you want to add:"))
-        milk_add = int(input("Write how many ml of milk do you want to add:"))
-        coffee_add = int(input("Write how many grams of coffee beans do you want to add:"))
-        disposable_cups_add = int(input("Write how many disposable cups of coffee do you want to add:"))
-        amounts["water"] += water_add
-        amounts["milk"] += milk_add
-        amounts["coffee_beans"] += coffee_add
-        amounts["disposable_cups"] += disposable_cups_add
-    elif options == "take":
-        print(f"I gave you ${amounts['money']}")
-        amounts["money"] = 0
-    elif options == "remaining":
-        supplies()
+            elif options == "fill":
+                water_add = int(input("Write how many ml of water do you want to add:"))
+                milk_add = int(input("Write how many ml of milk do you want to add:"))
+                coffee_add = int(input("Write how many grams of coffee beans do you want to add:"))
+                disposable_cups_add = int(input("Write how many disposable cups of coffee do you want to add:"))
+                self.water += water_add
+                self.milk += milk_add
+                self.coffee_beans += coffee_add
+                self.disposable_cups += disposable_cups_add
+            elif options == "take":
+                print(f"I gave you ${self.money}")
+                self.money = 0
+            elif options == "remaining":
+                print(f"""The coffee machine has:
+                {self.water} of water
+                {self.milk} of milk
+                {self.coffee_beans} of coffee beans
+                {self.disposable_cups} of disposable cups
+                {self.money} of money""")
 
 
-
-
-
-
-
-
-
+current_machine = CoffeeMachine(400, 540, 120, 9, 550)
+current_machine.start_machine()
